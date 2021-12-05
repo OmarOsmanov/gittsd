24e762ddfa148f60b8830cdb623c4aa598cc7fe6
OmarOsmanov <tu201906019@code.edu.az>
2021-12-05 20:11:34 +0400

    first commit

---
+++ tasktumorrow/script.js
@@ -0,0 +1,100 @@
+// function getAttributes()
+// {
+//  let u = document.getElementById("w3r").href;
+//  alert('The value of the href attribute of the link is : '+u);
+//  let v = document.getElementById("w3r").hreflang;   
+//  alert('The value of the hreflang attribute of the link is : '+v);
+//  let w = document.getElementById("w3r").rel; 
+//   alert('The value of the rel attribute of the link is : '+w);
+//  let x = document.getElementById("w3r").target; 
+//   alert('The value of the taget attribute of the link is : '+x);
+//  let y = document.getElementById("w3r").type; 
+//   alert('The value of the type attribute of the link is : '+y);  
+// }
+
+
+
+// task 2
+
+// function getFormvalue()
+// {
+//   let x=document.getElementById("form1");
+//    for (let i=0;i<x.length;i++)
+//     {
+//      if (x.elements[i].value!='Submit')
+//        {  
+//          console.log(x.elements[i].value);
+//        }  
+//     }
+// }
+
+
+// task3
+
+//   function insert_Row()
+//     {
+//        let x=document.getElementById('sampleTable').insertRow(0);
+//        let y = x.insertCell(0);
+//        let z = x.insertCell(1);
+//        y.innerHTML="New Cell1";
+//        z.innerHTML="New Cell2";
+// }
+
+
+//task 4
+//   function getOptions(){
+//   let x= document.getElementById("mySelect")
+//   let txt1 = "No. of items in dropdown is : ";
+//   let i;
+//   l=document.getElementById("mySelect").length;  
+//   txt1 = txt1+l;
+//     for (i=0;i<x.length;i++)
+//       {
+//          txt1 = txt1 + "\n" + x.options[i].text;
+//       }
+//     alert(txt1);
+//   }
+
+
+
+// task 5.1
+
+// let a =document.getElementById("bpx");
+// let b =document.getElementsByTagName("p")
+
+// a.style.backgroundColor="yellow";
+// a.style.border="5px green solid";
+// a.style.width="300px";
+// a.style.height="300px";
+// a.style.marginTop = "100px";
+// a.style.marginLeft ="150px";
+
+
+// task5.2
+
+// let a =document.getElementById("bmx");
+// a.style.border="5px red solid";
+// a.style.width="300px";
+// a.style.height="300px";
+// a.style.marginTop = "100px";
+// a.style.marginLeft ="150px";
+
+
+//task 5.3
+// let a =document.getElementById("btx");
+// a.style.border="5px green solid";
+// a.style.width="100px";
+// a.style.height="100px";
+// a.style.marginTop = "100px";
+// a.style.marginLeft ="150px";
+
+//task 5.4
+// let a =document.getElementById("bkx");
+// a.style.border="5px red solid";
+// a.style.width="320px";
+// a.style.height="320px";
+// a.style.marginTop = "100px";
+// a.style.marginLeft ="150px";
+
+
+
